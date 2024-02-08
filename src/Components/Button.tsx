e4bed6ace771a0c64a57a6107fb1387bb92e5407
PavanTank4631 <pavantank7016@gmail.com>
2024-02-08 08:45:17 +0530

    first commit

---
+++ src/Components/Button.tsx
@@ -0,0 +1,14 @@
+import React, { ChangeEvent, useState } from 'react'
+
+interface ButtonProps {
+  text?: string,
+  onClick?: () => void,
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return (
+    <></>
+  )
+}
+
+export default Button
